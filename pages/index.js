218a9b09b46e294f599fea5639808df2ca45c0cf
BeeChains <37987346+BeeChains@users.noreply.github.com>
2020-07-18 14:14:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to BHANDSHAKE <a href="https://nextjs.org">Next.js!</a>
       </h1>
-
+  
       <p className="description">
         Get started by editing <code>pages/index.js</code>
       </p>
